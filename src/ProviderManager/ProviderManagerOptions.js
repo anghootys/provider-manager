f2f1f70f676da015f4c607a4b8a9afabd8edcbf0
Sajjad Anghooty <anghootys@gmail.com>
2021-09-05 15:59:57 +0430

    fix (project) add some validations and error messages

--- src/ProviderManager/ProviderManagerOptions.js
+++ src/ProviderManager/ProviderManagerOptions.js
@@ -1,8 +1,24 @@
+const {OPTIONS_NOT_VALID, PROVIDERS_LIST_MISSING} = require("./Util/Constants/Errors");
+
 class ProviderManagerOptions {
     constructor(options) {
 
+        // if options list is empty of not valid should throw an error
+        if (!options) {
+            throw new Error(OPTIONS_NOT_VALID);
+        }
+
+        if (!options.providers) {
+            if (!options.ignoreErrors)
+                throw new Error(PROVIDERS_LIST_MISSING);
+
+            return false;
+        }
+
         // list of providers should be registered here
-        this._providers = options.providers;
+        this.providers = options.providers;
+        this.ignoreErrors = !!options.ignoreErrors;
+
     }
 }
 
--- src/ProviderManager/Util/Constants/Errors.js
+++ src/ProviderManager/Util/Constants/Errors.js
@@ -1,2 +1,3 @@
-module.exports.PROVIDERS_LIST_MISSING = "A list of providers should be defined as options."
+module.exports.OPTIONS_NOT_VALID = "list of options is not valid. please checkout readme at https://github.com/anghootys/provider-manager.";
+module.exports.PROVIDERS_LIST_MISSING = "A list of providers should be defined as options.";
 module.exports.PROVIDER_NOT_REGISTERED = "Provider % is not registered.";
